b5e9562caccf862a426983a4be0821a071735f35
control1kr <control1.kr@gmail.com>
2021-01-31 23:05:06 +0900

    Create AppendType.bat

---
+++ EQ/01.AppendType/AppendType.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+SET OUTFILE=EQStatus_FROM_EQ.txt
+SET INTERVAL=10
+
+IF EXIST %OUTFILE% DEL %OUTFILE%
+
+FOR /F "usebackq tokens=1,2* eol=; delims=," %%A IN (EQStatus.txt) DO (
+    TIMEOUT 1 > NUL
+    (ECHO %%A,%%B,%%C) >> %OUTFILE%
+)
+
+(ECHO a,,c) >> %OUTFILE%
